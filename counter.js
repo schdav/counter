b7149f724e5183f74dd6b2d625d5c077befbc31b
David Schneider <schdav@users.noreply.github.com>
2021-04-09 12:59:45 +0200

    Initial commit

---
+++ counter.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+  if (typeof Cookies.get('count') != 'undefined') {
+    $('#count').val(Cookies.get('count'));
+  } else {
+    Cookies.set('count', 0, { expires: 7, sameSite: 'strict' });
+    $('#count').val(Cookies.get('count'));
+  }
+
+  $('#up').click(function () {
+    $count = (parseInt(Cookies.get('count')) + 1).toString();
+    Cookies.set('count', $count, { expires: 7, sameSite: 'strict' });
+    $('#count').val(Cookies.get('count'));
+  });
+
+  $('#down').click(function () {
+    $count = (parseInt(Cookies.get('count')) - 1).toString();
+    Cookies.set('count', $count, { expires: 7, sameSite: 'strict' });
+    $('#count').val(Cookies.get('count'));
+  });
+
+  $('#reset').click(function () {
+    Cookies.set('count', 0, { expires: 7, sameSite: 'strict' });
+    $('#count').val(Cookies.get('count'));
+  })
+});
